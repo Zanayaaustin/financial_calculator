cb31fe9e9f3f7bb9f59e237ea2601c82808d431f
Zanaya Austin <zanayaaustin427@gmail.com>
2023-05-10 10:48:46 -0400

    Finance_Calc

---
+++ Finance_Calculator/calc.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// Grab HTML elements
+// Get number 1 input
+const numberOneInput = document.getElementById("numberOneInput");
+// Get number 2 input
+const numberTwoInput = document.getElementById("numberTwoInput");
+// Get answer input
+const answerOutput = document.getElementById("answerOutput");
+
+// Get the values
+function add() {
+  // Get the number 1 input value
+  const numberOne = Number(numberOneInput.value);
+  // Get the number 2 input value
+  const numberTwo = Number(numberTwoInput.value);
+  // Calculate the answer
+  const answer = numberOne + numberTwo;
+  // Set answer input to the answer
+  answerOutput.value =  answer;
+}
+
+function subtract() {
+  // Get the number 1 input value
+  const numberOne = Number(numberOneInput.value);
+  // Get the number 2 input value
+  const numberTwo = Number(numberTwoInput.value);
+  // Calculate the answer
+  const answer = numberOne - numberTwo;
+  // Set answer input to the answer
+  answerOutput.value =  answer;
+}
+
+function multiply() {
+  // Get the number 1 input value
+  const numberOne = Number(numberOneInput.value);
+  // Get the number 2 input value
+  const numberTwo = Number(numberTwoInput.value);
+  // Calculate the answer
+  const answer = numberOne * numberTwo;
+  // Set answer input to the answer
+  answerOutput.value =  answer;
+}
+
+function divide() {
+  // Get the number 1 input value
+  const numberOne = Number(numberOneInput.value);
+  // Get the number 2 input value
+  const numberTwo = Number(numberTwoInput.value);
+  // Calculate the answer
+  const answer = numberOne / numberTwo;
+  // Set answer input to the answer
+  answerOutput.value =  answer;
+}
